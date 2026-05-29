14f0859de92f171a1a759bee5bd35e914f64bd26
NikitaBaker <volk8390@gmail.com>
2026-05-29 17:54:47 +0300

    Add testing workflow

---
+++ test/example.test.js
@@ -0,0 +1,11 @@
+describe("Example tests", () => {
+
+  test("should pass", () => {
+    expect(1 + 1).toBe(2);
+  });
+
+  test("should handle strings", () => {
+    expect("hello".toUpperCase()).toBe("HELLO");
+  });
+
+});
